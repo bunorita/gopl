70f8beb27a032790a386e620e80f2517b75a4e6d
Buntaro.Orita <buntaro.orita@gmail.com>
2022-09-16 13:06:53 +0900

    ex7.3 *Tree.String()

--- ch04/treesort/treesort.go
+++ ch04/treesort/treesort.go
@@ -1,22 +1,42 @@
 package treesort
 
-type tree struct {
+import (
+	"strconv"
+	"strings"
+)
+
+type Tree struct {
 	value       int
-	left, right *tree
+	left, right *Tree
+}
+
+func NewTree(values []int) *Tree {
+	var root *Tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	return root
+}
+
+// ex 7.3
+func (t *Tree) String() string {
+	values := appendValues([]int{}, t)
+	sValues := []string{}
+	for _, v := range values {
+		sValues = append(sValues, strconv.Itoa(v))
+	}
+	return "{" + strings.Join(sValues, " ") + "}"
 }
 
 // Sort sorts values in place.
 func Sort(values []int) {
-	var root *tree
-	for _, v := range values {
-		root = add(root, v)
-	}
+	root := NewTree(values)
 	appendValues(values[:0], root)
 }
 
-func add(t *tree, value int) *tree {
+func add(t *Tree, value int) *Tree {
 	if t == nil {
-		return &tree{value: value}
+		return &Tree{value: value}
 	}
 	if value < t.value {
 		t.left = add(t.left, value)
@@ -28,7 +48,7 @@ func add(t *tree, value int) *tree {
 
 // appendValues appends the elements of t to values in order
 // and returns the resulting slice.
-func appendValues(values []int, t *tree) []int {
+func appendValues(values []int, t *Tree) []int {
 	if t != nil {
 		values = appendValues(values, t.left)
 		values = append(values, t.value)
--- ch04/treesort/treesort_test.go
+++ ch04/treesort/treesort_test.go
@@ -1,6 +1,7 @@
 package treesort_test
 
 import (
+	"fmt"
 	"math/rand"
 	"sort"
 	"testing"
@@ -19,5 +20,34 @@ func TestSort(t *testing.T) {
 	treesort.Sort(data)
 	if !sort.IntsAreSorted(data) {
 		t.Errorf("not sorted: %v\n", data)
+	}
+}
+
+// ex7.3
+func TestString(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		values []int
+	}{
+		{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}},
+		{[]int{8, 9, 6, 7, 4, 5, 2, 3, 0, 1}},
+		{[]int{5, 6, 7, 8, 9, 0, 1, 2, 3, 4}},
+		{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}},
+	}
+
+	want := "{0 1 2 3 4 5 6 7 8 9}"
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("values=%v", tt.values), func(t *testing.T) {
+			t.Parallel()
+
+			tr := treesort.NewTree(tt.values)
+			if got := tr.String(); got != want {
+				t.Errorf("got %s, want %s", got, want)
+			}
+		})
+
 	}
 }
